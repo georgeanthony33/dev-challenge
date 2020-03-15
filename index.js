7b828a60afc7a94f55c1705b7105fb105cfac8de
georgeanthony33 <georgeanthony33@gmail.com>
2020-03-15 14:12:48 +0000

    connected to mongo database and created environment file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-const port = process.env.PORT || 8000
+
+const { port, dbURI } = require('./config/environment')
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) return console.log(err)
+  console.log('Mongo is connected')
+})
 
 app.listen(port, () => console.log(`Up and running on port ${port}`))
