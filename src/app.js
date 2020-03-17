e74195c9cfaf00d96a12f56139f920001502a44e
georgeanthony33 <georgeanthony33@gmail.com>
2020-03-17 14:24:14 +0000

    added filter functionality for suppliers and products

--- src/app.js
+++ src/app.js
@@ -5,26 +5,62 @@ import './styles/main.scss'
 
 class App extends React.Component {
   state = {
-    productData: []
+    productData: [],
+    filteredData: [],
+    suppliersDropdown: [],
+    productsDropdown: [],
+    filterSelections: {
+      supplier: 'All',
+      product: 'All'
+    }
   }
 
   async componentDidMount() {
     try {
       const res = await axios.get('/api/products')
       const productData = res.data
-      this.setState({ productData })
+      const suppliersDropdown = Array.from(new Set(productData.map(product => (product.supplier.name))))
+      const productsDropdown = Array.from(new Set(productData.map(product => (product.name))))
+      this.setState({
+        productData,
+        suppliersDropdown,
+        productsDropdown
+      })
     } catch (err) {
       console.log(err)
     }
   }
 
+  handleChange = e => {
+    console.log(e.target.name, e.target.value)
+    const filterSelections = {
+      ...this.state.filterSelections,
+      [e.target.name]: e.target.value
+    }
+    this.setState({ filterSelections })
+  }
+
   render() {
-    console.log(this.state.productData)
+    console.log(this.state.filterSelections)
     if (!this.state.productData) return null
     return (
       <div className="frontpage-container">
         <div className="title-container">
           <h1>The Wongle People</h1>
+        </div>
+        <div className="inputs-container">
+          <select name="supplier" className="suppliers" onChange={this.handleChange}>
+            <option value="All">All</option>
+            {this.state.suppliersDropdown.map(supplier => (
+              <option key={supplier} value={supplier}>{supplier}</option>
+            ))}
+          </select>
+          <select name="product" className="products" onChange={this.handleChange}>
+            <option value="All">All</option>
+            {this.state.productsDropdown.map(product => (
+              <option key={product} value={product}>{product}</option>
+            ))}
+          </select>
         </div>
         <div className="products-container">
           <h2>Product details</h2>
@@ -37,7 +73,7 @@ class App extends React.Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.productData.map(product => (
+              {this.state.productData.filter(product => (product.supplier.name === this.state.filterSelections.supplier || this.state.filterSelections.supplier === 'All') && (product.name === this.state.filterSelections.product || this.state.filterSelections.product === 'All')).map(product => (
                 <tr key={product._id}>
                   <td>{product.supplier.name}</td>
                   <td>{product.name}</td>
