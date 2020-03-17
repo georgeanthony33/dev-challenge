7c35380665aa39f7483c96b12c062b51cce59d3d
georgeanthony33 <georgeanthony33@gmail.com>
2020-03-17 12:45:18 +0000

    product data rendering on page

--- src/app.js
+++ src/app.js
@@ -5,21 +5,49 @@ import './styles/main.scss'
 
 class App extends React.Component {
   state = {
-
+    productData: []
   }
 
   async componentDidMount() {
     try {
       const res = await axios.get('/api/products')
-      console.log(res)
+      const productData = res.data
+      this.setState({ productData })
     } catch (err) {
       console.log(err)
     }
   }
 
   render() {
+    console.log(this.state.productData)
+    if (!this.state.productData) return null
     return (
-      <h1>Hello</h1>
+      <div className="frontpage-container">
+        <div className="title-container">
+          <h1>The Wongle People</h1>
+        </div>
+        <div className="products-container">
+          <h2>Product details</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Supplier</th>
+                <th>Product</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.productData.map(product => (
+                <tr key={product._id}>
+                  <td>{product.supplier.name}</td>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     )
   }
 }
